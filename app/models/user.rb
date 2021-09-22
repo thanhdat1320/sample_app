033c48911cf77787ca6b91ba4762c34a88aba16a
thanhdat1320 <nhtd.1320@gmail.com>
2021-09-22 11:23:28 +0700

    Done chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-
+  attr_accessor :remember_token
   validates :name, :email, presence: true
   validates :password, length:
           {maximum: Settings.validations.length.digit_255,
@@ -10,12 +10,32 @@ class User < ApplicationRecord
                     minimum: Settings.validations.length.digit_6},
            format: {with: VALID_EMAIL_REGEX}
   has_secure_password
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost: cost
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
